e8ea82447fc95d0894b60952c3339f20072f3c30
Stefan Pahlplatz <stefan314@live.nl>
2017-07-22 20:54:39 +0200

    Part 3.

--- src/index.js
+++ src/index.js
@@ -5,10 +5,14 @@ import express from 'express';
 import constants from './config/constants';
 import './config/database';
 import middlewaresConfig from './config/middlewares';
+import apiRoutes from './modules';
 
 const app = express();
 
 middlewaresConfig(app);
+
+// Assign the routes to the app.
+apiRoutes(app);
 
 app.listen(constants.PORT, err => {
   if (err) {
